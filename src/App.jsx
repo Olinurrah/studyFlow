c8b95f244b23d293f56ea531280c4bf86b81c2d2
Olinurrah <olinurrahman96@gmail.com>
2026-09-18 08:25:00 +0600

    Initial project setup and instal relatet web technology

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+ <h1>بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</h1>
+ <h1>আসসালামু আলাইকুম </h1>
+ <h2>welcome to our study-flow </h2>
+
+    </>
+  )
+}
+
+export default App
